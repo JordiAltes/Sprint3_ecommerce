f3b39959dec65417031ca53ca2b717205e9e217e
JordiAltes <jordalan8@gmail.com>
2023-01-07 18:54:08 +0100

    exercici7 acabat

--- js/checkout.js
+++ js/checkout.js
@@ -1,9 +1,20 @@
 const validateEmail = (email) => {
 	return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   };
+
+  const addInvalidClass = (element, error) => {
+	if (error) {
+		element.classList.add("is-invalid")
+	}
+	else {
+		element.classList.remove("is-invalid")
+		element.classList.add("is-valid")
+	}
+  }
 // Exercise 6
 function validate() {
 	let error = 0;
+	let checkOutForm = document.getElementById("checkOutForm");
 	// Get the input fields
 	let fName = document.getElementById("fName");
 	let fEmail = document.getElementById("fEmail");
@@ -13,40 +24,66 @@ function validate() {
 	let fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
-	let errorName = document.getElementById("errorName");
+	/* let errorName = document.getElementById("errorName");
 	let errorEmail = document.getElementById("errorEmail");
 	let errorAddress = document.getElementById("errorAddress")
 	let errorLastN = document.getElementById("errorLastN");
 	let errorPassword = document.getElementById("errorPassword");
-	let errorPhone = document.getElementById("errorPhone");
+	let errorPhone = document.getElementById("errorPhone"); */
 	
 	// Validate fields entered by the user: name, phone, password, and email
 	if(fName.value === "" || !fName.value.match(/^[A-Za-z\s]*$/) || fName.value.length < 3){
+		addInvalidClass(fName, true)
 		error++;
+	}
+	else {
+		addInvalidClass(fName , false)
 	}
 
 	if(fEmail.value === "" || !validateEmail(fEmail.value) || fEmail.value.length < 3){
+		addInvalidClass(fEmail, true)
 		error++;
+	}
+	else {
+		addInvalidClass(fEmail , false)
 	}
 
 	if(fAddress.value === "" || fAddress.value.length < 3) {
+		addInvalidClass(fAddress, true)
 		error++
+	}
+	else {
+		addInvalidClass(fAddress , false)
 	}
 
 	if (fLastN.value === "" || !fLastN.value.match(/^[A-Za-z\s]*$/) || fLastN.value.length < 3) {
+		addInvalidClass(fLastN, true)
 		error++
+	}
+	else {
+		addInvalidClass(fLastN, false)
 	}
 	
-	if (fPhone.value === ""  || isNaN(fPhone.value) || fPhone.value.length !== 9) {
+	if (fPhone.value === "" || isNaN(fPhone.value) || fPhone.value.length !== 9) {
+		addInvalidClass(fPhone, true)
 		error++
 	}
+	else {
+		addInvalidClass(fPhone, false)
+	}
 
-	if(fPassword.value == "" ||  !fPassword.value.match(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{3,8}$/) || fPassword.value.length < 3){
+	if(fPassword.value == "" || !fPassword.value.match(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{3,8}$/) || fPassword.value.length < 3){
+		addInvalidClass(fPassword, true)
 		error++;
 	}
+	else {
+		addInvalidClass(fPassword, false)
+	}
 	 
-	if(error > 0){
-		alert("Error");
+	if(error>0){
+		checkOutForm.addEventListener('submit', e => {
+			e.preventDefault()
+		}, true)
 	}else{
 		alert("OK");
 	}
