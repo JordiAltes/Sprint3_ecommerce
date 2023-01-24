fa0a73a14ac646b5224b8fc4a6fdaba8d4fd80bb
JordiAltes <jordalan8@gmail.com>
2023-01-24 10:26:23 +0100

    formato el documento

--- js/checkout.js
+++ js/checkout.js
@@ -1,19 +1,19 @@
-function validateEmail (email) {
+function validateEmail(email) {
   return String(email)
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
-};
+}
 
-function addInvalidClass (element, error) {
+function addInvalidClass(element, error) {
   if (error) {
     element.classList.add("is-invalid");
   } else {
     element.classList.remove("is-invalid");
     element.classList.add("is-valid");
   }
-};
+}
 // Exercise 7
 function validate() {
   let error = 0;
@@ -86,12 +86,9 @@ function validate() {
   }
 
   if (error > 0) {
-    checkOutForm.addEventListener(
-      "submit",
-      (e) => {
-        e.preventDefault();
-      },
-    );
+    checkOutForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
   } else {
     alert("OK");
   }
